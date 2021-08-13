1b3fbd08c7a59a5ba6c438eb1642c6ca5cced83c
John A Stevenson <johnalexanderstevenson@gmail.com>
2021-08-13 17:35:46 +0100

    Update classifiers

--- setup.py
+++ setup.py
@@ -19,9 +19,13 @@ setup(
     author_email="johnalexanderstevenson@gmail.com",
     license="MIT",
     classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering :: Atmospheric Science"
     ],
     packages=["tephrange"],
     include_package_data=True,
